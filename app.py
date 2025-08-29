6f820a0df5be4614d3ef39d09ed50b50e846b993
rafaelccanutodev <rafaelccanuto@gmail.com>
2025-08-29 13:10:45 -0300

    alteracoes

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Restaurante da Praça', 'Comida Caseira')
-
-
+bebida_suco = Bebida('Suco de melancia',5.0,'Grande')
+prato_pao = Prato('Pão',2,'O melhor pão da cidade')
 
 
 def main():
-    Restaurante.listar_restaurantes()
-
+    
+    print(bebida_suco)
+    print(prato_pao)
 
 
 if __name__=="__main__":
